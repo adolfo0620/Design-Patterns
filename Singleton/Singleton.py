8ef6bc642bbe6c09499896f2bc9ba8724c21afa9
Adolfo Reyes <adolfo0620@gmail.com>
2017-06-15 18:49:14 -0400

    adding testcase and design

--- Singleton/Singleton.py
+++ Singleton/Singleton.py
@@ -1,6 +1,20 @@
-class Singleton():
-	singleton = None
-	def __init__(self):
-		if singleton == None:
-			self.singleton = Singleton()
-		return singleton
+import unittest
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class ObjectSingleton():
+	__metaclass__ = Singleton
+
+class TestSingleton(unittest.TestCase):
+	def test_singleton(self):
+		singleton_one = ObjectSingleton()
+		singleton_two = ObjectSingleton()
+		self.assertEqual(id(singleton_one), id(singleton_two))
+
+if __name__ == '__main__':
+    unittest.main() 
